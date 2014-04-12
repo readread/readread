bbc5d66c847fdf3cbc0f6f681896ba67dca11b60
Administrator <Administrator@roger-3ac61e5b5>
2014-04-12 23:00:53 +0800

    本地代码

--- readread/src/com/readread/article/constant/ReadConstant.java
+++ readread/src/com/readread/article/constant/ReadConstant.java
@@ -6,11 +6,11 @@ public final class ReadConstant {
 	public static final String DRIVER = "com.mysql.jdbc.Driver";
 	
 	// URL指向要访问的数据库名
-	public static final String URL = "jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX";
+	public static final String URL = "jdbc:mysql://127.0.0.1:3306/mysql";
 	
 	  // MySQL配置时的用户名
-	public static final String USER = "CLm02ATTIo2UIzbvDAu8G6ic"; 
+	public static final String USER = "root"; 
 
     // MySQL配置时的密码
-	public static final String PASSWORD = "bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU";
+	public static final String PASSWORD = "admin";
 }
